5a703ce3bb03d05fa06fd04aecff3c8adf818b70
Olga Biletska <olga.beletskaya@gmail.com>
2017-09-24 09:26:31 +1000

    Distance calculated by function

--- sandbox/src/main/java/MyFirstProg.java
+++ sandbox/src/main/java/MyFirstProg.java
@@ -1,8 +1,6 @@
 public class MyFirstProg {
 
     public static void main(String[] args) {
-        hello("world");
-        hello("you");
 
         Square s = new Square(7);
         System.out.println("area with length " + s.l + " equals " + s.area());
@@ -10,11 +8,21 @@ public class MyFirstProg {
         Rectangle r = new Rectangle(6, 9);
         System.out.println("rectangle area with length " + r.a + "and " + r.b + " equals " + r.area());
 
-    }
-    public static void hello(String somebody) {
-        System.out.println("Hello " + somebody + "?");
+
+        Point p1 = new Point();
+        Point p2 = new Point();
+        p1.x = 2;
+        p1.y = 3;
+        p2.x = 5;
+        p2.y = 4;
+        System.out.println("Distance between points P1(" + p1.x +"," + p1.y + ") and P2(" + p2.x + "," + p2.y + ") equals " + distance (p1, p2));
+
     }
 
+    public static double distance (Point p1, Point p2) {
 
+        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+
+    }
 }
 
