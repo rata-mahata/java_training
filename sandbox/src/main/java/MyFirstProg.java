d9915971b50ebd9d57d65c1b09d4d55745e28e85
Olga Biletska <olga.beletskaya@gmail.com>
2017-09-23 13:08:08 +1000

    Few functions added

--- sandbox/src/main/java/MyFirstProg.java
+++ sandbox/src/main/java/MyFirstProg.java
@@ -1,8 +1,26 @@
 public class MyFirstProg {
 
-public static void main (String[] args) {
-System.out.println("Hello world?");
-}
-}
+    public static void main(String[] args) {
+        hello("world");
+        hello("you");
+        double x = 4;
+        System.out.println("area with length " + x + " equals " + area(x));
 
+        double v=5;
+        double y=7;
+        System.out.println("rectangle area with length " + v + "and " + y + " equals " + area(v, y));
+
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "?");
+    }
+
+    public static double area(double l) {
+        return l*l;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
+    }
+}
 
