28c09b14ecc317fa99b05c3219412960efa89110
Olga Biletska <olga.beletskaya@gmail.com>
2017-09-23 17:28:26 +1000

    Methods added

--- sandbox/src/main/java/MyFirstProg.java
+++ sandbox/src/main/java/MyFirstProg.java
@@ -3,24 +3,18 @@ public class MyFirstProg {
     public static void main(String[] args) {
         hello("world");
         hello("you");
-        double x = 4;
-        System.out.println("area with length " + x + " equals " + area(x));
 
-        double v=5;
-        double y=7;
-        System.out.println("rectangle area with length " + v + "and " + y + " equals " + area(v, y));
+        Square s = new Square(7);
+        System.out.println("area with length " + s.l + " equals " + s.area());
+
+        Rectangle r = new Rectangle(6, 9);
+        System.out.println("rectangle area with length " + r.a + "and " + r.b + " equals " + r.area());
 
     }
     public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "?");
     }
 
-    public static double area(double l) {
-        return l*l;
-    }
 
-    public static double area(double a, double b) {
-        return a*b;
-    }
 }
 
---
+++ sandbox/src/main/java/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    double a;
+    double b;
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double area() {
+        return this.a * this.b;
+    }
+}
