0b2b1254232e019b4fd2d73ad7f752223efcd72b
Olga Biletska <olga.beletskaya@gmail.com>
2017-10-01 21:44:58 +1100

    Common TestBase class created for all tests

---
+++ addressbook-web-tests/src/test/java/addressbook/GroupDeletionTest.java
@@ -0,0 +1,19 @@
+package addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+
+    
+    @Test
+    public void testGroupDeletion() {
+
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroup();
+        returnToGroupPage();
+    }
+
+
+}
