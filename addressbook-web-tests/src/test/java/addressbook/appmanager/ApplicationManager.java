08755d9983b72b819dfe8370c6ea31f06d2c45a2
Olga Biletska <olga.beletskaya@gmail.com>
2017-10-02 23:31:18 +1100

    HelperBase class created

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -16,14 +16,7 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
 
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+
 
     public void init() {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
--- addressbook-web-tests/src/test/java/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ContactHelper.java
@@ -4,12 +4,11 @@ import addressbook.model.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactHelper {
+public class ContactHelper extends HelperBase {
 
-    private FirefoxDriver wd;
 
     public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void deleteSelectedContact() {
@@ -20,11 +19,12 @@ public class ContactHelper {
         if (!wd.findElement(By.name("selected[]")).isSelected()) {
             wd.findElement(By.name("selected[]")).click();
         }
-        //wd.selectContact();
+
     }
 
     public void returnToHomePage() {
-        wd.findElement(By.linkText("home")).click();
+        click(By.linkText("home"));
+
     }
 
     public void submitContactForm() {
@@ -32,21 +32,12 @@ public class ContactHelper {
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+        type(By.name("firstname"), contactData.getFirstname());
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("company"), contactData.getCompany());
+        type(By.name("address"), contactData.getAddress());
+        type(By.name("home"), contactData.getPhone());
+
     }
 
     public void initCreateContact() {
